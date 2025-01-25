5c006f1a34f84119499757eee01f76816eb5d2c4
Suman <suman@gmail.com>
2025-01-25 11:13:32 +0530

    Add Multiple filter

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,7 +18,7 @@ class UserController extends Controller
         if ($search) {
             $posts = array_filter($posts, function ($post) use ($search) {
                 //return stripos($post['title'], $search) !== false;
-				return stripos($post['title'], $search) !== false || stripos($post['body'], $search) !== false
+				return stripos($post['title'], $search) !== false || stripos($post['body'], $search) !== false;
             });
         }
 
@@ -40,9 +40,43 @@ class UserController extends Controller
 	}
 	
 	
-	
-	
-	
+	public function with_multiple_filter(Request $request){
+		// Fetch data from API
+        $response = Http::get('https://jsonplaceholder.typicode.com/posts');
+        $posts = $response->json();
+
+        // Get search inputs for title and body
+        $titleSearch = $request->input('title');
+        $bodySearch = $request->input('body');
+
+        // Filter posts based on title and body input fields
+        if ($titleSearch) {
+            $posts = array_filter($posts, function ($post) use ($titleSearch) {
+                return stripos($post['title'], $titleSearch) !== false;
+            });
+        }
+
+        if ($bodySearch) {
+            $posts = array_filter($posts, function ($post) use ($bodySearch) {
+                return stripos($post['body'], $bodySearch) !== false;
+            });
+        }
+
+        // Paginate the filtered data
+        $perPage = 10; // Number of items per page
+        $currentPage = $request->input('page', 1);
+        $offset = ($currentPage - 1) * $perPage;
+
+        $paginatedData = new LengthAwarePaginator(
+            array_slice($posts, $offset, $perPage),
+            count($posts),
+            $perPage,
+            $currentPage,
+            ['path' => url()->current()]
+        );
+
+        return view('table_with_filter', compact('paginatedData', 'titleSearch', 'bodySearch'));
+	}
 	
 	
     public function index_without_filter(Request $request){
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\UserController;
 
 Route::get('/', [UserController::class, 'index']);
 
+Route::get('/table_with_multiple_filter', [UserController::class, 'with_multiple_filter']);
+
 /*Route::get('/', function () {
     return view('welcome');
 });
